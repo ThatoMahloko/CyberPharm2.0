ef4940090df9beb46d4331ab318e8d5d565c7f2d
ThatoMahloko <thato732mahloko@gmail.com>
2022-01-18 10:36:44 +0200

    contact form

--- App.js
+++ App.js
@@ -24,7 +24,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-       
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Vaccination" component={Vaccination} />
         <Stack.Screen name="Doctors" component={Doctors} />
